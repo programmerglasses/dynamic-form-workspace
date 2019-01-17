cfcb9353146d43c30be13fc63a440cf588826e56
Arturas <a.ochonko@gmail.com>
2019-01-17 20:48:15 +0200

    initial commit

---
+++ src/components/Root/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default () => (
+  <div className="wrapper">
+    <h1 className="typography">Dynamic form workspace.</h1>
+  </div>
+);
