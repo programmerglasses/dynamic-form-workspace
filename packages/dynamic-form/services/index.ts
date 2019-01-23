3ea788278fe24175ebb036a9e178ed7675f960fe
Arturas <a.ochonko@gmail.com>
2019-01-23 13:20:09 +0200

    dynamic-form: created render priority provider service class

--- packages/dynamic-form/services/index.ts
+++ packages/dynamic-form/services/index.ts
@@ -1,3 +1,4 @@
 export * from './attribute-copier.service';
 export * from './attribute-parser.service';
 export * from './event-parser.service';
+export * from './render-priority-provider.service';
---
+++ packages/dynamic-form/services/render-priority-provider.service.ts
@@ -0,0 +1,56 @@
+import { ConfigurationItem, RenderPriorityProviderOptions } from '../interfaces';
+import { SettingsInspector } from '../utils';
+
+export class RenderPriorityProviderService {
+  private readonly configuration: ConfigurationItem[];
+  private readonly settingsInspector: SettingsInspector;
+
+  constructor(private readonly options: RenderPriorityProviderOptions) {
+    this.configuration = [
+      ...options.configuration
+    ];
+    this.settingsInspector = new SettingsInspector();
+  }
+
+  findHighestRenderPriority() {
+    const configurationItemRenderPriorities: number[] = [];
+
+    this.configuration.forEach((configurationItem: ConfigurationItem) => {
+      const hasRenderPriorityProperty = this.settingsInspector.hasRenderPriorityProperty(configurationItem);
+
+      if (hasRenderPriorityProperty) {
+        const renderPriority = configurationItem.settings.renderPriority;
+        configurationItemRenderPriorities.push(renderPriority);
+      }
+    });
+
+    return Math.max.apply(this, configurationItemRenderPriorities);
+  }
+
+  provideRenderPriority(configurationItem: ConfigurationItem, configurationItemPosition: number, highestRenderPriority: number) {
+    if (!this.settingsInspector.hasSettingsProperty(configurationItem)) {
+      configurationItem.settings = {};
+    }
+
+    if (!this.settingsInspector.hasRenderPriorityProperty(configurationItem)) {
+      configurationItem.settings.renderPriority = highestRenderPriority + configurationItemPosition;
+    }
+  }
+
+  verifyConfigurationItems() {
+    const highestRenderPriority = this.findHighestRenderPriority();
+
+    return this.configuration.map((configurationItem: ConfigurationItem, index: number) => {
+      const configurationItemPosition = ++index;
+      return this.verifyRenderPriority(configurationItem, configurationItemPosition, highestRenderPriority);
+    });
+  }
+
+  verifyRenderPriority(configurationItem: ConfigurationItem, configurationItemPosition: number, highestRenderPriority: number) {
+    if (!this.settingsInspector.hasRenderPriorityProperty(configurationItem)) {
+      this.provideRenderPriority(configurationItem, configurationItemPosition, highestRenderPriority);
+    }
+
+    return configurationItem;
+  }
+}
