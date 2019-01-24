e6529b0125e04b742f2498d15ebe0af0622e57f7
Arturas <a.ochonko@gmail.com>
2019-01-24 10:53:11 +0200

    dynamic-form: created configuration parser service class

---
+++ packages/dynamic-form/services/configuration-parser.service.ts
@@ -0,0 +1,80 @@
+import { ComponentFactory } from '../components';
+import { ComponentType } from '../enums';
+import { ConfigurationItem, ConfigurationParserOptions, Parsable } from '../interfaces';
+import { AttributeParserService } from './attribute-parser.service';
+import { EventParserService } from './event-parser.service';
+import { RenderPriorityProviderService } from './render-priority-provider.service';
+import { RenderPrioritySorterService } from './render-priority-sorter.service';
+
+export class ConfigurationParserService implements Parsable {
+  private readonly configuration: ConfigurationItem[];
+  private readonly componentFactory: ComponentFactory;
+  private readonly destinationElement: Element;
+
+  constructor(private readonly options: ConfigurationParserOptions) {
+    this.configuration = [
+      ...options.configuration
+    ];
+    this.componentFactory = new ComponentFactory();
+    this.destinationElement = <Element> options.destinationElement.cloneNode(true);
+  }
+
+  getConfigurationItems(renderType: string) {
+    return this.configuration
+      .filter((configurationItem: ConfigurationItem) => configurationItem.render === renderType);
+  }
+
+  getSortedConfigurationByRenderPriority() {
+    const buttonRenderPriorityProviderService = new RenderPriorityProviderService({
+      configuration: this.getConfigurationItems(ComponentType.BUTTON_WRAPPER)
+    });
+
+    const formControlRenderPriorityProviderService = new RenderPriorityProviderService({
+      configuration: this.getConfigurationItems(ComponentType.INPUT_WRAPPER)
+    });
+
+    const buttonRenderPrioritySorterService = new RenderPrioritySorterService({
+      configuration: buttonRenderPriorityProviderService.verifyConfigurationItems()
+    });
+
+    const formControlRenderPrioritySorterService = new RenderPrioritySorterService({
+      configuration: formControlRenderPriorityProviderService.verifyConfigurationItems()
+    });
+
+    const sortedButtonConfigurationItems = buttonRenderPrioritySorterService.sort();
+    const sortedFormControlConfigurationItems = formControlRenderPrioritySorterService.sort();
+
+    return sortedFormControlConfigurationItems.concat(sortedButtonConfigurationItems);
+  }
+
+  parse(): Element {
+    const sortedConfigurationByRenderPriority = this.getSortedConfigurationByRenderPriority();
+
+    sortedConfigurationByRenderPriority.forEach((configurationItem: ConfigurationItem) => {
+      const attributes = configurationItem.attributes;
+      const events = configurationItem.events;
+
+      let customComponent = this.componentFactory.create(configurationItem.render);
+
+      if (attributes) {
+        const attributeParserService = new AttributeParserService({
+          attributes,
+          destinationElement: customComponent
+        });
+        customComponent = <HTMLElement> attributeParserService.parse();
+      }
+
+      if (events) {
+        const eventParserService = new EventParserService({
+          events,
+          destinationElement: customComponent
+        });
+        customComponent = <HTMLElement> eventParserService.parse();
+      }
+
+      this.destinationElement.appendChild(customComponent);
+    });
+
+    return this.destinationElement;
+  }
+}
--- packages/dynamic-form/services/index.ts
+++ packages/dynamic-form/services/index.ts
@@ -1,5 +1,6 @@
 export * from './attribute-copier.service';
 export * from './attribute-parser.service';
+export * from './configuration-parser.service';
 export * from './event-parser.service';
 export * from './render-priority-provider.service';
 export * from './render-priority-sorter.service';
