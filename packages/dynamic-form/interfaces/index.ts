0c2b65918242b2ea81c9ed78c93a8e7c38fa3743
Arturas <a.ochonko@gmail.com>
2019-01-28 21:46:32 +0200

    dynamic-form: created event type interface

--- packages/dynamic-form/interfaces/index.ts
+++ packages/dynamic-form/interfaces/index.ts
@@ -4,6 +4,7 @@ export * from './attribute-type.interface';
 export * from './configuration-item.interface';
 export * from './configuration-parser-options.interface';
 export * from './event-parser-options.interface';
+export * from './event-type.interface';
 export * from './parsable.interface';
 export * from './render-priority-provider-options.interface';
 export * from './render-priority-sorter-options.interface';
